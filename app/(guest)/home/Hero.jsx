51b56246747ab39f0153c6665e413203eead715b
David Ayres <davidrayres@hotmail.com>
2024-07-20 12:06:39 -0400

    verbage edits

--- app/(guest)/home/Hero.jsx
+++ app/(guest)/home/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
       <div className='absolute flex flex-col text-center text-blue-600 transform translate-x-[-50%] left-1/2 top-2 w-full'>
         <h1 className='text-4xl font-extrabold sm:text-5xl md:text-6xl'>86 W Second</h1>
         <h2 className='mt-4 mb-2 text-2xl font-bold'>Your Ocean Isle Beach Experience</h2>
-        <p>5 BEDS | 5 BATHS | 12 GUESTS</p>
+        <p>5 BEDS | 5 BATHS | 12+ GUESTS</p>
       </div>
     </section>
   )
